242889df1060662231fd85bfeb339c68c20574b6
Oliver <monoxane@gmail.com>
2016-01-28 13:37:00 +1100

    updated autolinking

--- PolyBot_MP_2.py
+++ PolyBot_MP_2.py
@@ -65,9 +65,12 @@ while True:
 		if action == 'PRIVMSG':
 
 			if data.find('v/') != -1:
- 				match = re.search('v/(\w*)', data)
-				for group in match.groups():
-					Send(str('[voat.co/v/') + group + str(']'))
+				if data.find('http://voat.co/' or 'https://voat.co/')
+					print('full URL, not linking')
+				else
+ 					match = re.search('v/(\w*)', data)
+					for group in match.groups():
+						Send(str('[voat.co/v/') + group + str(']'))
 				
 			if data.find('PolyBot, ') != -1:
 				x = data.split('#')[1]
