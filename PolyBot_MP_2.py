7cdfee940e1ab8256de92ed9a92b1fd5175d4b46
Oliver <monoxane@gmail.com>
2016-01-28 13:39:48 +1100

    Update PolyBot_MP_2.py

--- PolyBot_MP_2.py
+++ PolyBot_MP_2.py
@@ -67,7 +67,7 @@ while True:
 			if data.find('v/') != -1:
 				if data.find('http://voat.co/' or 'https://voat.co/'):
 					print('full URL, not linking')
-				else
+				else:
  					match = re.search('v/(\w*)', data)
 					for group in match.groups():
 						Send(str('[voat.co/v/') + group + str(']'))
