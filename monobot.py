0eb67aba77a79cc7caec050c7db05ff39ab2c93b
Oliver <monoxane@gmail.com>
2016-01-29 18:56:22 +1100

    Added the begining of es and en responses and triggers and fixed syntax

--- monobot.py
+++ monobot.py
@@ -22,7 +22,7 @@ irc.send ( 'USER monobot monobot monobot :monoxane Python IRC bot\r\n' )
 #---------------------------------- Functions -------------------------------------#
 
 def Send(chan, msg):
-    irc.send('PRIVMSG ' + chan + ' :' + msg +  '\r\n')
+    irc.send('PRIVMSG #' + chan + ' :' + msg +  '\r\n')
 
 def Join(channel):
     irc.send ( 'JOIN ' + channel + '\r\n' )
@@ -30,20 +30,20 @@ def Join(channel):
 def Part(chan):
     irc.send ( 'PART ' + chan + '\r\n' )
 
-def GetMsg()
+def GetMsg():
 	x = data.split('#')[1]
 	x = x.split(':')[1]
 	info[0] = info[0].strip(' \t\n\r')
 	return(message)
 
-def GetNick()
+def GetNick():
     nick = data.split('!')[0]
-	nick = nick.replace(':', ' ')
+    nick = nick.replace(':', ' ')
     nick = nick.replace(' ', '')
     nick = nick.strip(' \t\n\r')
     return nick
 
-def UserJoins()
+def UserJoins():
     time.sleep(0.5)
     wb = random.choice(["Welcome Back", "Hello", "Welcome"])
     GetNick(data)
@@ -55,15 +55,17 @@ def UserJoins()
             Send('Welcome To The Matrix, ' +nick)
             open(joinlog, 'a').write(data)
 
-def ChanLang()
+def ChanLang():
     GetChan()
-    if chan == "hira"
+    if chan == "hira":
         lang = 'es'
-    else
+    elif chan == 'bah':
+        lang = 'en,es'
+    else:
         lang = 'en'
 
 
-def GetChan()
+def GetChan():
 	chan1 = data.split('PRIVMSG ')[1]
 	chan1 = chan1.replaec(':', ' ')
     chan1 = chan1.replace(' ', '')
@@ -101,12 +103,22 @@ while True:
 		if action == 'PRIVMSG':
 
             GetMessage(data)
-            if message = info
+            if message = info:
                 GetChan()
                 ChanLang()
-                if lang == 'en'
-                    if chan == 'modernpowers'
-                        Send(chan, 'This is the IRC channel for v/modernpowers')
+                if lang == 'en':
+                    if chan == 'modernpowers':
+                        Send('modernpowers', 'This is the IRC channel for v/modernpowers')
+                    if chan == 'gaming':
+                        Send('gaming', 'This is the official IRC of v/gaming')
+                elif lang == 'es':
+                    if chan == 'hira':
+                        send('hira', 'Este es el principal canal Hira.io . Diviértete.')
+                elif lang == 'es,en':
+                    if chan == 'bah':
+                        Send('bah', 'This is the goat.chat help channel, IRCops are opped (@,%,~) or voiced (+) ask them any questions')
+                        Send('bah', 'Este es el canal de ayuda goat.chat , IRCops es op ( @ , % , ~ ) o expresaron ( + ) hacerles cualquier pregunta que pueda tener.
+')
 
         if action == 'JOIN':
             Joins()
